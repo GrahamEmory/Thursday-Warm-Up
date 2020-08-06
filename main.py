d413ff2350b98bc006ece113ee053de2caa1a040
GrahamEmory <gemory44@gmail.com>
2020-08-06 19:36:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,71 @@
+# VARIABLES - stores a single piece of data. 
+
+a = 2 #integer data type = is a number that can be used for math
+b = "2" #string data type = is a word that can not be used for math
+
+#  print(a+a) 
+#  print(b+b) 
+
+# What are the 2 characteristics of data? VALUE AND DATA TYPE
+
+# LOOP - Repeats code for a given amount of time or until a condition is met.
+
+# For loop
+# for i in range(5):
+#   print("It's almost friday", i)
+
+# while True
+# smelly = True
+# while smelly == True:
+#   print("I need a bath")
+#   smelly = False
+
+# counter = 0
+# while counter < 5:
+#   print(counter)
+#   counter += 1
+
+# Conditionals - IF/ELSE 
+# if counter == 0:
+#   print("equals 0")
+# elif counter > 5:
+#   print("greater than 5")
+# else:
+#   print("less than 5")
+
+#1. Give me an example of a string variable and an integer variable.
+#2. Create your OWN for loop 
+#3. Create your own while loop 
+#4. In a comment, provide an example of when to use:
+    # a) a variable
+    # b) a loop
+    # c) a conditionals
+#5. Provide any questions that you currently have or provide a concept you're confused about.
+      
+  
+H = 5
+F = 2
+print(H-F)
+
+A = "Goodbye"
+B = "Friend"
+print(A + " " + B)
+
+for i in range(2,8):
+  print("Is this annoying?")
+
+Word = True
+while Word == True:
+  print("This is not supposed to be here")
+  Word = False
+
+while Word == False:
+  print("Oh wait. I guess it is.")
+  Word = True
+
+# A) If you want to identify a letter, word, or a number with a single piece of data that you want to use later in your project, utilizing variables is incredibly helpfull.
+# B) If you want to repeat a certain line of code a certain time, or only if a certain status is on or not, a for loop and a while loop can make that possible.
+# C) If you are wanting to make certain possibilities in your project possible, using conditional statements that utlizize if, elif, and else all together can make that super simple and easy to do.
+
+# 5) What is the difference between functions and while loops that use True and False statements to operate the loop? Are they the same thing?
+
